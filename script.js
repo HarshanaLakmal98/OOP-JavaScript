4a5fd48e1b8051397f2a8b7464a4541ecdd1b309
Lakmal <harshanalakmal.work@gmail.com>
2023-02-04 11:27:55 +0530

    create fullName method in Person Class

--- script.js
+++ script.js
@@ -24,3 +24,23 @@ class User{
 // crate object using class
 const userOne = new User('Harshana',1234,"harshana123@gamil.com");
 console.log(userOne);
+
+//  ======================= add method to class ====================
+
+class Person{
+    constructor(fname,lname,age){
+        this.firstName = fname;
+        this.lastName = lname;
+        this.age = age;
+    }
+    // create method
+    fullName(){
+        return `Full Name is : ${this.firstName} ${this.lastName}`
+    }
+}
+// crate personOne object using person class
+
+const personOne = new Person('Harshana',"lakmal",25);
+console.log(personOne);
+// called method 
+console.log(personOne.fullName());
